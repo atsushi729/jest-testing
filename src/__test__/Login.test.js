aa611161c7b9803426e57c6ca110f2694e9abba7
Atsushi <atu729@icloud.com>
2024-03-28 12:37:14 +0900

    add validation test

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,11 +1,30 @@
 import { render, screen } from "@testing-library/react";
-import Login from "../Login";
+import Login, { validateEmail } from "../Login";
 
 describe("test Login componetn", () => {
+  // Check button element in Login component
   test("should first", async () => {
     render(<Login />);
-
-    const button = await screen.findAllByRole("button");
+    const button = await screen.findAllByRole("button"); // return as promise object
     expect(button).toHaveLength(1);
+  });
+
+  // Check password type
+  test("password type should be password", () => {
+    render(<Login />);
+    const password = screen.getByPlaceholderText("Please enter password");
+    expect(password).toHaveAttribute("type", "password");
+  });
+
+  // Check email validation to fail if invalid email is inputed
+  test("should be faild in email validation", () => {
+    const testEmail = "test.com";
+    expect(validateEmail(testEmail)).not.toBe(true);
+  });
+
+  // Check email validation to success if valid email is inputed
+  test("should be successed in email validation", () => {
+    const testEmail = "test@gmail.com";
+    expect(validateEmail(testEmail)).toBe(true);
   });
 });
